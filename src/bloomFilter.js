8c62acb80f44c44f5b44ec4295b94fa5f2e23b60
Cassandra Cruz <celkamada@gmail.com>
2015-03-09 21:40:45 -0700

    Implemented hashing functions and bloom filter

--- src/bloomFilter.js
+++ src/bloomFilter.js
@@ -2,13 +2,26 @@ var BloomFilter = function(length, hashingFunctions){
   this._length = length || 100;
   this._hashingFunctions = hashingFunctions;
   this._data = new Array(size);
-}
+};
 
 BloomFilter.prototype.insert = function(value){
+  var key = [];
 
-}
+  for (var i = 0; i < 3; i++) {
+    key[i] = hashingFunctions[i](this._length, value);
+    this._data[key[i]] = 1;
+  }
+
+};
 
 BloomFilter.prototype.contains = function(value){
+  var key = [];
 
-}
+  for (var i = 0; i < 3; i++) {
+    key[i] = hashingFunctions[i](this._length, value);
+  }
 
+  return key.reduce(function(current, position) {
+    return this._data[position] === 1 ? current : false;
+  }, true);
+};
--- src/hashingFunctions.js
+++ src/hashingFunctions.js
@@ -1,13 +1,24 @@
 var hashingFunction1 = function(range, value){
-
-}
+  return value.length % range;
+};
 
 var hashingFunction2 = function(range, value){
+  var result = value.split('');
 
-}
+  result =  (result.map(function(value) {
+    return value.charCodeAt(0);
+  }).map(function(value) {
+    return (value ^ range);
+  }).reduce(function(a, b) {
+    return a + b ;
+  })) ^ 255;
+
+  console.log(result % range, range);
+  return result % range;
+};
 
 var hashingFunction3 = function(range, value){
-
-}
+  return (value.length ^ 255) % range;
+};
 
 var hashingFunctions = [hashingFunction1, hashingFunction2, hashingFunction3];
