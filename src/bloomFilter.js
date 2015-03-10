a83134d9d1319ddaa875822c72c4694e22c90169
Drew Cuthbertson <syeoryn@gmail.com>
2015-03-10 02:21:56 -0700

    Implement hash function and bloom filter solution

--- src/bloomFilter.js
+++ src/bloomFilter.js
@@ -5,10 +5,19 @@ var BloomFilter = function(length, hashingFunctions){
 }
 
 BloomFilter.prototype.insert = function(value){
-
+  for(var i = 0; i < this._hashingFunctions.length; i++){
+    var hash = this._hashingFunctions[i](this._length, value);
+    this._data[hash] = 1;
+  }
 }
 
 BloomFilter.prototype.contains = function(value){
-
+  for(var i = 0; i < this._hashingFunctions.length; i++){
+    var hash = this._hashingFunctions[i](this._length, value);
+    if(!this._data[hash]){
+      return false;
+    }
+  }
+  return true;
 }
 
--- src/hashingFunctions.js
+++ src/hashingFunctions.js
@@ -1,13 +1,34 @@
 var hashingFunction1 = function(range, value){
-
+  // rotating hash
+  var total = 0
+  for(var i = 0; i < value.length; i++){
+    total = (total  << 4) | (total >> 28) | value.charCodeAt(i);
+  }
+  return Math.abs(total % range);
 }
 
 var hashingFunction2 = function(range, value){
+  // Jenkins Hash-- One at a time hash
+  var total = 0
+  for(var i = 0; i < value.length; i++){
+    total += value.charCodeAt(i);
+    total += total << 10;
+    total ^= total >> 6;
+  }
+  total += total << 3;
+  total ^= total >> 11;
+  total += total << 15;
 
+  return Math.abs(total % range);
 }
 
 var hashingFunction3 = function(range, value){
-
+  // Modified XOR hash
+  var total = 0;
+  for(var i = 0; i < value.length; i++){
+    total ^= value.charCodeAt(i);
+  }
+  return total % range;
 }
 
 var hashingFunctions = [hashingFunction1, hashingFunction2, hashingFunction3];
